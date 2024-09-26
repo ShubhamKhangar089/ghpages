44f7ae14e3bd32d91a5fc4dbaecfcc06ec80d33d
unknown <shubhamkhangar45@gmail.com>
2024-09-26 19:50:42 +0530

    update code1.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -6,9 +8,15 @@ function App() {
 
   return (
     <>
-     
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
       <h1>Vite + React</h1>
-      <div>This is Updated</div>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
